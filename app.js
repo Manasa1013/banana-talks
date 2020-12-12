9bfb34a51cccc43c43f8ca19f5fc34a9c27faa6c
Manasa1013 <66269691+Manasa1013@users.noreply.github.com>
2020-12-13 00:40:46 +0530

    changes to js file

---
+++ app.js
@@ -0,0 +1,33 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var textInput = document.querySelector("#text-input");
+var outputDiv = document.querySelector("#output-div");
+
+function constructMOckURL(text){
+    return "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json" + "?" +"text=" + text;
+} //noted down &constructed the url path
+
+function constructURL(text){
+    return "https://api.funtranslations.com/translate/minion.json" +"?" + "text=" +text;
+}
+
+function errorHandler(error){
+    console.log("Error occured from server ",error )
+    alert("Server error ,try again with new text")
+}
+function doFetch(text){
+    fetch(constructURL(text)).then(response =>response.json())
+    .then(json =>outputDiv.innerText=json.contents.translated).catch(errorHandler);
+}
+function clickEventHandler(){
+    console.log("clicked");
+//     outputDiv.innerText  = textInput.value+"  clicked translate";
+// }
+//declare a var or const to hold the text input value
+const inputText = textInput.value;
+doFetch(inputText);
+
+}
+
+btnTranslate.addEventListener("click",clickEventHandler);
+
+
